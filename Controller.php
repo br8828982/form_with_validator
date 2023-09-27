758153c525c0621797a74040369a166ccfc98ddc
br8828982 <141433491+br8828982@users.noreply.github.com>
2023-09-28 05:20:02 +0530

    Create Controller.php

---
+++ Controller.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once('Validator.php');
+require_once('User.php');
+
+class Controller
+{
+    private $validator;
+    private $user;
+
+    public function __construct($data)
+    {
+        $this->validator = new Validator($data);
+        $this->user = new User($data);
+    }
+  
+    public function handleForm()
+    {
+        $rules = [
+            'username' => ['required', 'min:6', 'unique'],
+            'email' => ['required', 'email', 'unique'],
+            'password' => ['required', 'min:8'],
+        ];
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $this->validator->validate($rules);
+
+
+            if ($this->validator->passes()) {
+                $this->user->register();
+                echo "User registration successful!";
+            }
+        }
+    }
+
+    public function getValidator()
+    {
+        return $this->validator;
+    }
+}
